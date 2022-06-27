7f9f033ee597b23a4c8a3991d5ec32fff656df8a
Ochanyoro <wadaoo0223@icloud.com>
2022-06-27 19:46:25 +0900

    Initial Commit

---
+++ BuildNetflixApp_perfect/BuildNetflixApp_perfectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildNetflixApp_perfectApp.swift
+//  BuildNetflixApp_perfect
+//
+//  Created by 大和田一裕 on 2022/06/27.
+//
+
+import SwiftUI
+
+@main
+struct BuildNetflixApp_perfectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuildNetflixApp_perfect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BuildNetflixApp_perfect
+//
+//  Created by 大和田一裕 on 2022/06/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
